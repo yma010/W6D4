294274e007a7e57838a17b98a6fac48ef217e93c
Marvin Ma <yma010@ucr.edu>
2019-08-08 09:10:01 -0700

    Board.rb updated

--- Self_Study/Memory_Puzzle/board.rb
+++ Self_Study/Memory_Puzzle/board.rb
@@ -1,2 +1,16 @@
-class Board
+class Board #This should keep track of the board, and initialize the board grid
+    def initialize(size = 4)
+        @size = size
+        @grid = Array.new(size) {Array.new(size)}
+    end
+
+    def [] pos
+        row, col = pos #pos is an array of [x, y]
+        @grid[row][col]
+    end
+
+    def [] = pos, val #Should set a val to a position
+        row, col = pos
+        @grid[pos] = val
+    end
 end
